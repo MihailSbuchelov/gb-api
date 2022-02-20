bd536427a50e564ae776fde73b670048acd43607
MikhailSbuchelov <mikivolkoff@ya.ru>
2022-02-20 10:53:27 +0300

    up lib

---
+++ src/main/java/ru/gb/api/orders/dto/OrdersDto.java
@@ -0,0 +1,53 @@
+package ru.gb.api.orders.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.*;
+import ru.gb.api.common.enums.OrdersStatus;
+import ru.gb.api.product.dto.ProductDto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrdersDto {
+
+    @JsonProperty(value = "id")
+    private Long ordersId;
+    private String address;
+    @NotBlank
+    private String firstname;
+    @NotBlank
+    private String lastname;
+    @NotBlank
+    private String phone;
+    @Email
+    private String mail;
+    private OrdersStatus status;
+    @JsonFormat(pattern = "dd.MM.yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate deliveryDate;
+    @NotEmpty
+    private Set<ProductDto> products;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime createdDate;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime lastModifiedDate;
+}
